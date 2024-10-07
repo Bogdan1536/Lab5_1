b98e5d1b6fd559eeba0b4e9646035899c207d110
Богдан Пашківський <bogdanpaskivskij7@gmail.com>
2024-10-07 20:34:23 +0300

    Добавьте файлы проекта.

---
+++ Lab5_1.cpp
@@ -0,0 +1,45 @@
+﻿// 9. Підрахувати середнє арифметичне всіх додатних елементів у даному одновимірному масиві.//
+
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int size;
+    int* array; 
+    double ser = 0;
+    double sum = 0, k = 0;
+
+    cout << "Введіть розмір масиву: ";
+    cin >> size;
+    srand(time(0));
+
+    // Створення динамічного масиву
+    array = new int[size];
+    cout << "   Масив:" << endl;
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 21 - 10;
+        cout << array[i] << " ";
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (array[i] > 0) {
+            sum += array[i];
+            k +=1;
+        }
+    }
+    ser+= sum / k;
+    cout << "\nCереднє арифметичне всіх додатних елементів у даному одновимірному масиві: " << ser;
+    cout << endl;
+
+    delete[] array;
+
+    return 0;
+}
+
